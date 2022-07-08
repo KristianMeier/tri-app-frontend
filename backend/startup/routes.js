c9ac059aeb6f3b76f6fe9b358124fc112d9dc050
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-07-08 10:32:49 +0200

    refractor

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const genres = require("../routes/genres");
-const movies = require("../routes/movies");
+const practices = require("../routes/practices");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/genres", genres);
-  app.use("/api/movies", movies);
+  app.use("/api/movies", practices);
 };
