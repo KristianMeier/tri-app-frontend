e42229fa2953463b6e5ee071d8ca7ab10eb45790
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-07-08 10:15:22 +0200

    fjernet error og validation

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ const app = express();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
-require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const genres = require("../routes/genres");
 const movies = require("../routes/movies");
-const error = require("../middleware/error");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/genres", genres);
   app.use("/api/movies", movies);
-  app.use(error);
 };
