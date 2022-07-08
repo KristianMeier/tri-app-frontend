acb6edffb1e55c57bdeb34d2292127587cabad57
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-07-08 09:53:24 +0200

    fjernelse af validation

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -22,21 +22,6 @@ class Form extends Component {
     );
   }
 
-  renderSelect(name, label, options) {
-    const { data, errors } = this.state;
-
-    return (
-      <Select
-        name={name}
-        value={data[name]}
-        label={label}
-        options={options}
-        onChange={this.handleChange}
-        error={errors[name]}
-      />
-    );
-  }
-
   renderInput(name, label, type = "text") {
     const { data } = this.state;
 
